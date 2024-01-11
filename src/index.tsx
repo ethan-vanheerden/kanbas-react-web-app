8a00407a79bba7c418bbf3e57c60a42b670c6dd4
Ethan van Heerden <ethan@vpoint.biz>
2024-01-10 21:17:55 -0500

    small commit

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import React from 'react';
 //     <App />
 //   </React.StrictMode>
 // );
+//
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
