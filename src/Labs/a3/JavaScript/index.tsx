a5c8da834c82d0942c46bc87029ba4747ced37ba
Ethan van Heerden <ethan@vpoint.biz>
2024-02-07 14:35:14 -0500

    wip 2.2.6)

---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,22 @@
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import BooleanVariables from "./variables/BooleanVariables";
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+
+function JavaScript() {
+  console.log('Hello World!');
+  return (
+    <div>
+      <h1>JavaScript</h1>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <WorkingWithFunctions />
+    </div>
+  );
+}
+export default JavaScript
