2f56183de782afc4355017468c66af26ba2bdc03
Ethan van Heerden <ethan@vpoint.biz>
2024-02-07 20:28:00 -0500

    lab done

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,12 +1,27 @@
+import ConditionalOutput from "./ConditionalOutput";
+import Classes from "./css/Classes";
+import Styles from "./css/Styles";
+import Highlight from "./Highlight";
 import JavaScript from "./JavaScript";
+import Add from "./Add";
 import PathParameters from "./routing/PathParameters";
+import TodoList from "./todo/TodoList";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <TodoList />
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
       <JavaScript />
       <PathParameters />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+      <Add a={3} b={4} />
     </div>
   );
 }
