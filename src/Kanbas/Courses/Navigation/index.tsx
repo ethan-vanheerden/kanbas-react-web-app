5e3560830773e7b2729fcb3004ebf6d6d6cf4ebe
Ethan van Heerden <ethan@vpoint.biz>
2024-02-09 00:53:12 -0500

    wip course home module db

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,22 +1,40 @@
-import { Link, useLocation } from "react-router-dom";
-import "./index.css"; // feel free to use the CSS from previous assignments
+import { Link, useLocation } from 'react-router-dom'
+import './index.css' // feel free to use the CSS from previous assignments
+
+export const courseLinks = [
+  'Home',
+  'Modules',
+  'Piazza',
+  'Zoom Meetings',
+  'Assignments',
+  'Quizzes',
+  'Grades',
+  'People',
+  'Panopto Video',
+  'Discussions',
+  'Announcements',
+  'Pages',
+  'Files',
+  'Rubrics',
+  'Outcomes',
+  'Collaborations',
+  'Syllabus',
+  'Settings',
+]
 
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Zoom Meetings",
-    "Assignments", "Quizzes", "Grades", "People",
-    "Panopto Video", "Discussions", "Announcements", "Pages",
-    "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus",
-    "Settings"];
-  const { pathname } = useLocation();
+  const { pathname } = useLocation()
   return (
     <ul className="wd-navigation">
-      {links.map((link, index) => (
-        <li key={index} className={(decodeURI(pathname)).includes(link) ? "wd-active" : ""}>
+      {courseLinks.map((link, index) => (
+        <li
+          key={index}
+          className={decodeURI(pathname).includes(link) ? 'wd-active' : ''}
+        >
           <Link to={link}>{link}</Link>
         </li>
       ))}
     </ul>
-  );
+  )
 }
 export default CourseNavigation;
-
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,27 +1,56 @@
-import { courses } from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
-import { HiMiniBars3 } from "react-icons/hi2";
-import CourseNavigation from "./Navigation";
-import "../styles.css";
-import "./index.css";
+import { courses } from '../../Kanbas/Database'
+import {
+  Navigate,
+  Route,
+  Routes,
+  useParams,
+  Link,
+  useLocation,
+} from 'react-router-dom'
+import { HiMiniBars3 } from 'react-icons/hi2'
+import CourseNavigation, { courseLinks } from './Navigation'
+import '../styles.css'
+import './index.css'
 
 function Courses() {
-  const { courseId } = useParams();
-  const course = courses.find((course) => course._id === courseId);
+  const { courseId } = useParams()
+  const location = useLocation()
+  const path = decodeURI(location.pathname).split("/");
+  const getBreadcrumb = () => {
+    const breadcrumbTexts =  courseLinks.filter(link => path.includes(link));
+    return breadcrumbTexts.length > 0 ? breadcrumbTexts[0] : "";
+  }
+  const breadcrumb = getBreadcrumb();
+
+  const course = courses.find((course) => course._id === courseId)
   return (
     <div>
       <div className="ms-4">
         <div className="wd-course-title wd-fg-red">
-          <HiMiniBars3 className="me-3" /> {course?.number}
+          <HiMiniBars3 className="me-3" />
+          <ul className="breadcrumb">
+            <Link
+              className="breadcrumb-item wd-breadcrumb-item first"
+              to="Home"
+            >
+              {course?.number}
+            </Link>
+            <Link
+              className="breadcrumb-item wd-breadcrumb-item active"
+              to={breadcrumb}
+            >
+              {breadcrumb}
+            </Link>
+          </ul>
         </div>
         <hr />
       </div>
 
-
       <CourseNavigation />
       <div
         className="overflow-y-scroll position-fixed bottom-0 end-0"
-        style={{ left: "320px", top: "50px" }} >
+        style={{ left: '320px', top: '50px' }}
+      >
         <Routes>
           <Route path="/" element={<Navigate to="Home" />} />
           <Route path="Home" element={<h1>Home</h1>} />
@@ -29,7 +58,10 @@ function Courses() {
           <Route path="Piazza" element={<h1>Piazza</h1>} />
           <Route path="Zoom Meetings" element={<h1>Zoom Meetings</h1>} />
           <Route path="Assignments" element={<h1>Assignments</h1>} />
-          <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
+          <Route
+            path="Assignments/:assignmentId"
+            element={<h1>Assignment Editor</h1>}
+          />
           <Route path="Quizzes" element={<h1>Quizzes</h1>} />
           <Route path="Grades" element={<h1>Grades</h1>} />
           <Route path="People" element={<h1>People</h1>} />
@@ -45,8 +77,7 @@ function Courses() {
           <Route path="Settings" element={<h1>Settings</h1>} />
         </Routes>
       </div>
-
     </div>
-  );
+  )
 }
-export default Courses;
+export default Courses
