5dabc271189f01d4d3d3b80b8201c8b1855bc669
Ethan van Heerden <ethan@vpoint.biz>
2024-02-22 18:55:23 -0500

    wip

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
-import { courses } from '../Database'
+import db from '../Database'
 import { FaBook } from 'react-icons/fa'
 import { HiMiniBars3 } from 'react-icons/hi2'
 import './index.css'
@@ -8,8 +8,55 @@ import '../styles.css'
 import CollapsedKanbasNavigation from '../Navigation/Collapsed'
 import useCollapseNav from '../hooks/useCollapseNav'
 
-function Dashboard() {
+type Course = {
+  _id: string;
+  name: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+  color: string;
+};
+
+function Dashboard({ courses, course, setCourse, addNewCourse,
+  deleteCourse, updateCourse
+}:
+  {
+    counter: number;
+    setCounter: (counter: number) => void;
+  }) {
   const [collapsedKanbasNavOpen, setCollapsedKanbasNavOpen] = useState(false)
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "../../images/reactjs.jpg", color: "blue"
+  });
+
+  const addNewCourse = () => {
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString()
+    };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   const handleToggleCollapsedKanbasNav = () => {
     setCollapsedKanbasNavOpen((prevState) => !prevState)
   }
@@ -21,9 +68,8 @@ function Dashboard() {
   return (
     <div>
       <div
-        className={`wd-kanbas-slide-nav ${
-          collapsedKanbasNavOpen ? '' : 'wd-kanbas-slide-closed'
-        }`}
+        className={`wd-kanbas-slide-nav ${collapsedKanbasNavOpen ? '' : 'wd-kanbas-slide-closed'
+          }`}
       >
         <CollapsedKanbasNavigation
           closeAction={handleToggleCollapsedKanbasNav}
@@ -45,6 +91,22 @@ function Dashboard() {
       </div>
       <div className="p-4">
         <h1>Dashboard</h1> <hr />
+        <h5>Add or Edit a Course</h5>
+        <input value={course.name} className="form-control mb-1"
+          onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+        <input value={course.number} className="form-control mb-1"
+          onChange={(e) => setCourse({ ...course, number: e.target.value })} />
+        <input value={course.startDate} className="form-control mb-1" type="date"
+          onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
+        <input value={course.endDate} className="form-control mb-1" type="date"
+          onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
+        <button className="wd-course-button-red" onClick={addNewCourse} >
+          Add
+        </button>
+        <button className="wd-course-button" onClick={updateCourse} >
+          Update
+        </button>
+
         <h2>Published Courses (7)</h2> <hr />
         <div className="row">
           <div className="row row-cols-1 row-cols-md-5 g-4">
@@ -70,11 +132,31 @@ function Dashboard() {
                       {course.name}{' '}
                     </Link>
                     <p className="card-text">{course.number}</p>
-                    <Link to={`/Kanbas/Courses/${course._id}/Assignments`}>
-                      <div className="wd-kanbas-course-icons">
-                        <FaBook />
+                    <div className="wd-course-buttons-bottom">
+                      <Link to={`/Kanbas/Courses/${course._id}/Assignments`}>
+                        <div className="wd-kanbas-course-icons">
+                          <FaBook />
+                        </div>
+                      </Link>
+
+                      <div className="justify-content-end wd-edit-delete-buttons">
+                        <button className="wd-course-button"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            setCourse(course);
+                          }}>
+                          Edit
+                        </button>
+
+                        <button className="wd-course-button"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            deleteCourse(course._id);
+                          }}>
+                          Delete
+                        </button>
                       </div>
-                    </Link>
+                    </div>
                   </div>
                 </div>
               </div>
--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -2,4 +2,7 @@ import courses from './courses.json'
 import modules from './modules.json'
 import assignments from './assignments.json'
 
+const db = { courses, modules, assignments }
+
+export default db;
 export { courses, modules, assignments }
