cff6629778fb41ef75493e2f77ee3caece0f0e63
Ethan van Heerden <ethan@vpoint.biz>
2024-02-23 11:30:57 -0500

    cleanup

--- src/Labs/a4/BooleanStateVariables.tsx
+++ src/Labs/a4/BooleanStateVariables.tsx
@@ -6,7 +6,7 @@ function BooleanStateVariables() {
     <div>
       <h2>Boolean State Variables</h2>
       <p>{done ? "Done" : "Not done"}</p>
-      <label className="form-control">
+      <label className="form-control mb-2">
         <input type="checkbox" checked={done} onChange={() => setDone(!done)} style={{ marginRight: '10px' }} />
         Done
       </label>
